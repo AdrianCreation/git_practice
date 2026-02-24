e1064af89dac71dcdd18e0bc7ea0396f87e89761
achavez <81263787+achavez1988@users.noreply.github.com>
2026-02-23 22:06:14 -0600

    Fixes to Shi,

--- StatsigFE/view/frontend/web/requirejs-config.js
+++ StatsigFE/view/frontend/web/requirejs-config.js
@@ -3,20 +3,12 @@
  */
 var config = {
     paths: {
-        'statsig-sdk': "https://cdn.jsdelivr.net/npm/@statsig/js-client@3/build/statsig-js-client+session-replay+web-analytics.min.js?apikey=YOUR_CLIENT_KEY"
+        'statsig-js-cdn': 'https://cdn.jsdelivr.net/npm/@statsig/js-client@3/build/statsig-js-client+session-replay+web-analytics.min',
+        'statsig': 'RightPoint_StatsigFE/js/vendor/statsig-manager'
     },
     shim: {
-        'statsig-sdk': {
-            exports: 'statsig',
-            init: function() {
-                // Ensure the global statsig is available 
-                return window.statsig || window.Statsig || this.statsig;
-            }
-        }
-    },
-    map: {
-        '*': {
-            'statsig-sdk': 'RightPoint_StatsigFE/js/statsig_manager'
+        'statsig-js-cdn': {
+            exports: 'Statsig'
         }
     }
 };
